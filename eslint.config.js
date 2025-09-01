23d9f27ed6c8d95ad71ac977285b00cfa50a3f78
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-09-01 19:34:35 +0800

    eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  eslintConfigPrettier,
+  pluginReact.configs.flat.recommended,
+]);
