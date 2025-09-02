5f847c3096b6c1ad46ce515495f43ca763103a3f
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-02 22:51:54 +0800

    basic team section component

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,26 @@
-import { TeamSection } from "~/team-section/team-section";
+import { TeamSection } from "~/components/team-section";
 
+const testData = {
+  team: "Men's Basketball",
+  title: "RESTORING ORDER",
+  blurb:
+    "BUILT ON years of excellence, the Ateneo Men’s Basketball Team has long operated under a standard of winning. Despite the roadblock of missing the Final Four for the first time in 11 years, the Blue Eagles return with the same mission: to assert their place among the UAAP’s elite.",
+};
 export function Welcome() {
   return (
     <>
-      <TeamSection />
-      <TeamSection />
+      <TeamSection
+        team={testData.team}
+        title={testData.title}
+        blurb={testData.blurb}
+        alignment="right"
+      />
+      <TeamSection
+        team={testData.team}
+        title={testData.title}
+        blurb={testData.blurb}
+        alignment="left"
+      />
     </>
   );
 }
