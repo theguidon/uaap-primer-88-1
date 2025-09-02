af212489235ea6eda82dac8aef3adbeeff0594fe
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-02 23:04:55 +0800

    fix alignment styles

--- app/components/team-section.tsx
+++ app/components/team-section.tsx
@@ -14,18 +14,18 @@ export function TeamSection({
   const restBlurb = splitBlurb.slice(2).join(" ");
   return (
     <section
-      className={`flex flex-col h-dvh lg:${alignment == "right" ? "flex-row" : "flex-row-reverse"} lg:h-[50dvh] border-red-500 border-2`}
+      className={`flex flex-col h-dvh ${alignment == "right" ? "lg:flex-row" : "lg:flex-row-reverse"} lg:h-[50dvh] border-red-500 border-2`}
     >
       <div className="bg-black flex-6 relative">
         image
         <h2
-          className={`text-white hidden font-display text-4xl absolute w-min text-${alignment} top-0 ${alignment}-0 p-[5rem] font-bold lg:block`}
+          className={`text-white hidden font-display text-4xl absolute w-min ${alignment == "right" ? "text-right" : "text-left"} top-0 ${alignment == "right" ? "right-0" : "left-0"} p-[5rem] font-bold lg:block`}
         >
           {team}
         </h2>
       </div>
       <div
-        className={`text-center p-8 text-uaap-blue flex-4 flex flex-col lg:text-${alignment == "right" ? "right" : "left"} lg:px-20 lg:py-10`}
+        className={`text-center p-8 text-uaap-blue flex-4 flex flex-col ${alignment == "right" ? "lg:text-right" : "lg:text-left"} lg:px-20 lg:py-10`}
       >
         <h2 className="capitalize font-display font-bold lg:hidden">{team}</h2>
         <h2 className="uppercase text-[3.25rem] font-display font-bold leading-[85%] my-2 lg:text-[8.75rem] lg:my-4 lg:mt-0">
@@ -35,12 +35,15 @@ export function TeamSection({
           <span className="font-bold uppercase">{firstTwoBlurb} </span>
           {restBlurb}
         </div>
-        <button
-          className={`flex self-center gap-1 upppercase py-2 px-3.5 bg-uaap-dark-blue text-white font-body font-bold mt-4 lg:text-2xl lg:mt-6 lg:self-${alignment == "right" ? "end" : "start"}`}
+        <a
+          href=""
+          className={`self-center mt-4 cursor-pointer lg:mt-6 ${alignment == "right" ? "lg:self-end" : "lg:self-start"}`}
         >
-          READ MORE
-          <div>{">"}</div>
-        </button>
+          <button className="flex gap-1 py-2 px-3.5 upppercase bg-uaap-dark-blue text-white font-body font-bold lg:text-2xl">
+            READ MORE
+            <div>{">"}</div>
+          </button>
+        </a>
       </div>
     </section>
   );
