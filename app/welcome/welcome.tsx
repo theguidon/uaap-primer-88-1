50a5ef4742c2ecfd6e639f86703465d101833a6b
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-09-02 11:39:51 +0800

    title section html

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,8 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 import { useMediaQuery } from "react-responsive";
-import anims from "./anims.css";
+import "./anims.css";
+import styles from "./home.module.css";
 
 function BloomingCircles({ outerSize, radiiGap, colors, animStagger, animSpeed, delay }: { outerSize: string, radiiGap: string, colors: string[], animStagger: number, animSpeed: string, delay: number }) {
   // note that outerSize must be divisible by radiiGap...
@@ -50,24 +51,20 @@ export function Welcome() {
 
   return (
     <main>
-      <div className="graphic">
-        <BloomingCircles
-          outerSize="500px"
-          radiiGap="50px"
-          colors={["#b7e1f2", "#56b8e1", "#0e9ad4"]}
-          animStagger={100}
-          animSpeed="350ms"
-          delay={0}
-        />
-        <BloomingCircles
-          outerSize="300px"
-          radiiGap="30px"
-          colors={["rgba(249, 218, 44, 0.3)", "rgba(249, 218, 44, 0.7)", "#f9da2c"]}
-          animStagger={100}
-          animSpeed="350ms"
-          delay={30}
-        />
-      </div>
+      <section className={styles.title}>
+        <div>
+          <img src="/Long_Logo.svg" />
+        </div>
+        <div className={styles.titleUAAP}>
+          <h1>UAAP</h1>
+        </div>
+        <div className={styles.titleSeason}>
+          <h1>SEASON 88</h1>
+        </div>
+        <div className={styles.titleSemester}>
+          <h1>First Semester Primer</h1>
+        </div>
+      </section>
     </main>
   );
 }
