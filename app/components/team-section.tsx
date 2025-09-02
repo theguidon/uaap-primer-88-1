e7ad3b0cc854ab77ba1cdda9dd94313f9fec2bf0
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-03 00:53:38 +0800

    decrease title size (some titles do not fit)

--- app/components/team-section.tsx
+++ app/components/team-section.tsx
@@ -24,21 +24,31 @@ export function TeamSection({
         <h2
           className={`text-white hidden font-display text-4xl absolute w-min ${alignment == "right" ? "text-right" : "text-left"} top-0 ${alignment == "right" ? "right-0" : "left-0"} p-[5rem] font-bold lg:block`}
         >
-          {team}
+          {team.split(" ").length > 2 ? (
+            <>
+              {/* puts sports that are two words or more in one line */}
+              {team.split(" ").slice(0, 1)}{" "}
+              <p className="w-max">{team.split(" ").slice(1).join(" ")}</p>
+            </>
+          ) : (
+            team
+          )}
         </h2>
       </div>
       <div
-        className={`text-center p-8 text-uaap-blue flex-4 flex flex-col ${alignment == "right" ? "lg:text-right" : "lg:text-left"} lg:px-20 lg:py-0 lg:self-center`}
+        className={`text-center p-8 text-uaap-blue flex-4 flex flex-col justify-between h-full ${alignment == "right" ? "lg:text-right" : "lg:text-left"} lg:p-20 lg:self-center`}
       >
-        <h2 className="capitalize font-display font-bold text-[2rem] lg:hidden">
-          {team}
-        </h2>
-        <h2 className="uppercase text-[3.25rem] font-display font-bold leading-[85%] my-2 lg:text-[8.75rem] lg:my-4 lg:mt-0">
-          {title}
-        </h2>
-        <div className="text-sm font-body lg:text-base">
-          <span className="font-bold uppercase">{firstTwoBlurb} </span>
-          {restBlurb}
+        <div>
+          <h2 className="capitalize font-display font-bold text-[2rem] lg:hidden">
+            {team}
+          </h2>
+          <h2 className="uppercase text-[3.25rem] font-display font-bold leading-[85%] my-2 lg:text-[6rem] lg:my-4 lg:mt-0">
+            {title}
+          </h2>
+          <p className="text-sm font-body lg:text-base">
+            <span className="font-bold uppercase">{firstTwoBlurb} </span>
+            {restBlurb}
+          </p>
         </div>
         <a
           href={`/${slug}`}
