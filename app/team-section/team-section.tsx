877187b0a4672a0462b8aec141c58244666caed3
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-02 19:01:01 +0800

    mobile template styles

--- app/team-section/team-section.tsx
+++ app/team-section/team-section.tsx
@@ -2,9 +2,9 @@ export function TeamSection() {
   return (
     <section className="flex flex-col h-dvh lg:flex-row lg:h-[50dvh]">
       <div className="bg-black h-[60dvh]">image</div>
-      <div className="text-center p-8 lg:text-right">
-        <h2 className="capitalize font-display">Men's Basketball</h2>
-        <h2 className="uppercase text-[3.25rem] font-display leading-[85%] my-2">
+      <div className="text-center p-8 lg:text-right text-uaap-blue">
+        <h2 className="capitalize font-display font-bold">Men's Basketball</h2>
+        <h2 className="uppercase text-[3.25rem] font-display font-bold leading-[85%] my-2">
           RESTORING ORDER
         </h2>
         <div className="text-sm font-body">
@@ -14,7 +14,9 @@ export function TeamSection() {
           time in 11 years, the Blue Eagles return with the same mission: to
           assert their place among the UAAP’s elite.
         </div>
-        <button className="upppercase">READ MORE</button>
+        <button className="upppercase py-2 px-3.5 bg-uaap-dark-blue text-white font-body font-bold mt-4">
+          READ MORE
+        </button>
       </div>
     </section>
   );
