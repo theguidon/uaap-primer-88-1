b33c5d4d0ec34651f8b466f29bdf6a7a4fbc937d
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-03 01:48:02 +0800

    align sport on image to the grid (i love grid theory wow)

--- app/components/team-section.tsx
+++ app/components/team-section.tsx
@@ -17,12 +17,12 @@ export function TeamSection({
   return (
     <section
       id={slug} // use this as anchor for navigation sidebar
-      className={`flex flex-col h-dvh ${alignment == "right" ? "md:flex-row" : "md:flex-row-reverse"} md:h-[50dvh]`}
+      className={`flex flex-col h-dvh ${alignment == "right" ? "md:flex-row" : "md:flex-row-reverse"} md:h-[50dvh] relative`}
     >
-      <div className="bg-black flex-6 relative">
+      <div className="bg-black flex-6">
         image
         <h2
-          className={`text-white hidden font-display text-4xl absolute w-min ${alignment == "right" ? "text-right" : "text-left"} top-0 ${alignment == "right" ? "right-0" : "left-0"} p-[2.5rem] font-bold xl:block`}
+          className={`text-white hidden font-display font-bold text-4xl absolute w-min ${alignment == "right" ? "text-right" : "text-left"} top-0 ${alignment == "right" ? "right-0" : "left-0"} p-8 md:block md:px-[calc(60dvw-2rem)] lg:py-15 xl:py-10 xl:px-[calc(60dvw-6rem)]`}
         >
           {team.split(" ").length > 2 ? (
             <>
@@ -36,23 +36,23 @@ export function TeamSection({
         </h2>
       </div>
       <div
-        className={`text-center p-8 text-uaap-blue flex-4 flex flex-col justify-between h-full lg:p-15 ${alignment == "right" ? "xl:text-right" : "xl:text-left"} xl:px-20 xl:py-10 xl:self-center`}
+        className={`text-center p-8 text-uaap-blue flex-4 flex flex-col justify-between h-full sm:p-15 md:p-8 ${alignment == "right" ? "md:text-right" : "md:text-left"} lg:py-15 xl:px-20 xl:py-10 xl:self-center`}
       >
         <div>
-          <h2 className="capitalize font-display font-bold text-[2rem] xl:hidden">
+          <h2 className="capitalize font-display font-bold text-[2rem] mb-2 md:hidden">
             {team}
           </h2>
-          <h2 className="uppercase text-[3.25rem] font-display font-bold leading-[85%] my-2 xl:text-[6rem] xl:my-4 xl:mt-0">
+          <h2 className="uppercase text-[3.25rem] font-display font-bold leading-[85%] mb-2 sm:text-[4rem] md:text-[4.5rem] lg:text-[5rem] xl:text-[6rem] xl:mb-4">
             {title}
           </h2>
-          <p className="text-sm font-body xl:text-base">
+          <p className="text-sm font-body max-w-96 lg:text-base m-auto md:max-w-none">
             <span className="font-bold uppercase">{firstTwoBlurb} </span>
             {restBlurb}
           </p>
         </div>
         <a
           href={`/${slug}`}
-          className={`self-center mt-4 xl:mt-6 ${alignment == "right" ? "xl:self-end" : "xl:self-start"}`}
+          className={`self-center mt-4 ${alignment == "right" ? "md:self-end" : "md:self-start"} xl:mt-6`}
         >
           <button className="flex gap-1 p-2.5 cursor-pointer upppercase bg-uaap-dark-blue text-white font-body font-bold xl:text-2xl xl:p-4">
             READ MORE
