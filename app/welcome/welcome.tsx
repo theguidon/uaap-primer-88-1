c3796e7db50140828d4e4a1998aefe7d8a8fbbac
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-09-02 21:42:42 +0800

    animated title

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,15 +1,20 @@
 import { useMediaQuery } from "react-responsive";
 import "./anims.css";
 import styles from "../pages/home.module.css";
-import { motion, MotionConfig } from "motion/react";
+import { motion } from "motion/react";
 import BloomingCircles from "~/components/BloomingCircles";
 
 function EnterButton() {
   return (
-    <div className={styles.enterButton}>
+    <motion.div
+      initial={{ width: 0, padding: 0 }}
+      animate={{ width: 120, padding: 12 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      className={styles.enterButton}
+    >
       <p>Enter</p>
       <img src="/Arrow up-right.svg" />
-    </div>
+    </motion.div>
   );
 }
 
@@ -20,14 +25,41 @@ export function Welcome() {
   return (
     <main>
       <section className={styles.title}>
+        <div>
+          <motion.img
+            src="/Long_Logo.svg"
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            style={{ height: 25, marginBottom: 8 }}
+          />
+        </div>
         <div className={styles.titleUAAP}>
-          <h1>UAAP</h1>
+          <motion.h1
+            initial={{ y: "-100%" }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.6, ease: "easeInOut" }}
+          >
+            UAAP
+          </motion.h1>
         </div>
         <div className={styles.titleSeason}>
-          <h1>SEASON 88</h1>
+          <motion.h1
+            initial={{ y: "-100%" }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.7, ease: "easeInOut", delay: 0.1 }}
+          >
+            SEASON 88
+          </motion.h1>
         </div>
         <div className={styles.titleSemester}>
-          <h1>First Semester Primer</h1>
+          <motion.h1
+            initial={{ y: "-100%" }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.8, ease: "easeInOut", delay: 0.2 }}
+          >
+            First Semester Primer
+          </motion.h1>
         </div>
         <EnterButton />
       </section>
