cf8420737e24c2ef9c95527fa3fc6a8b81f8f94f
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-03 12:50:21 +0800

    change data schema

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,14 +4,14 @@ import data from "../assets/data/data.json";
 export function Welcome() {
   return (
     <>
-      {data.map(({ team, title, article, slug }, idx) => (
+      {Object.entries(data).map(([key, { team, title, article }], idx) => (
         <TeamSection
-          key={slug}
+          key={key}
           team={team}
           title={title}
           blurb={article.split("\n\n")[0]} // get first paragraph of article
           alignment={idx % 2 == 0 ? "right" : "left"}
-          slug={slug}
+          slug={key}
         />
       ))}
     </>
