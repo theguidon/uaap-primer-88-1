0f42a7d9ee000d627cb341092b6ce7b0a6a100a5
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-03 16:48:49 +0800

    responsiveness

--- app/pages/write-up.tsx
+++ app/pages/write-up.tsx
@@ -17,32 +17,36 @@ export async function loader({ params }: Route.LoaderArgs) {
   return articleData[params.slug];
 }
 
-export default function WriteUp({ loaderData }: Route.ComponentProps) {
+export default function WriteUp({ params, loaderData }: Route.ComponentProps) {
   const { team, title, byline, article } = loaderData;
   return (
     <div>
-      <nav className="absolute w-dvw top-24 px-24 flex justify-between">
+      <nav className="absolute w-dvw top-20 px-20 flex justify-between">
         <div className="text-white">hamburger</div>
-        <button className="flex text-white font-body font-bold text-2xl items-center p-6 bg-uaap-dark-blue">
-          <IconContext.Provider
-            value={{
-              style: { color: "white", scale: "-1 1" },
-              size: "1.5rem",
-            }}
-          >
-            <MdArrowOutward />
-          </IconContext.Provider>
-          BACK
-        </button>
+        <NavLink to={`/#${params.slug}`}>
+          <button className="cursor-pointer flex gap-1 text-white font-body font-bold text-2xl items-center p-5 bg-uaap-dark-blue">
+            <IconContext.Provider
+              value={{
+                style: { color: "white", scale: "-1 1" },
+                size: "1.875rem",
+              }}
+            >
+              <MdArrowOutward />
+            </IconContext.Provider>
+            BACK
+          </button>
+        </NavLink>
       </nav>
-      {/* bg image */}
-      <div className="bg-black w-full h-[70dvh]" />
-      <section className="max-w-[52rem] absolute top-36 left-1/2 translate-x-[-50%]">
-        <h2 className="font-display font-bold text-4xl text-white">{team}</h2>
-        <h1 className="font-display font-bold text-9xl text-white uppercase">
+      {/* bg image placeholder */}
+      <div className="bg-black w-full h-[60dvh]" />
+      <section className="max-w-[50rem] w-full px-10 absolute top-44 left-1/2 translate-x-[-50%]">
+        <h2 className="font-display font-bold text-3xl text-white sm:text-4xl">
+          {team}
+        </h2>
+        <h1 className="font-display font-bold text-8xl text-white uppercase leading-[85%] sm:text-9xl">
           {title}
         </h1>
-        <h3 className="text-white mb-4">By {byline}</h3>
+        <h3 className="text-white mt-2 mb-4">By {byline}</h3>
         {/* image placeholder */}
         <div className="w-full aspect-[1.5] bg-uaap-blue" />
         <div className="font-body text-uaap-blue mt-[4rem] mb-[6rem]">
