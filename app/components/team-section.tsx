b5880dccb8577ca79951532bd6eb34527a8b5ff2
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-04 08:51:09 +0800

    better mask

--- app/components/team-section.tsx
+++ app/components/team-section.tsx
@@ -41,56 +41,60 @@ export function TeamSection({
       className={`snap-start flex flex-col h-dvh relative overflow-hidden ${alignment == "right" ? "md:flex-row" : "md:flex-row-reverse"} md:h-[50dvh]`}
     >
       <div className="bg-black flex-6">image</div>
-      <div className="absolute hidden bg-black z-10 h-1/3 top-0 right-[calc(40dvw+2.5rem)] w-[calc(100dvw-(40dvw+2.5rem)*2)] md:block xl:right-[calc(40dvw+6rem)] xl:w-[calc(100dvw-(40dvw+6rem)*2)]" />
-      <motion.h2
-        initial={{
-          translateX: alignment == "right" ? "20%" : "-20%",
-        }}
-        whileInView={{
-          transition: { duration: 0.75 },
-          translateX: "0",
-        }}
-        className={`text-white hidden font-display font-bold text-5xl absolute w-min ${alignment == "right" ? "text-right" : "text-left"} top-8 ${alignment == "right" ? "right-0" : "left-0"} px-8 md:block md:px-[calc(60dvw-2.5rem)] lg:top-15 xl:top-10 xl:px-[calc(60dvw-6rem)] 2xl:top-14`}
+      <div
+        className={`overflow-hidden text-white hidden font-display font-bold text-5xl absolute w-min ${alignment == "right" ? "text-right" : "text-left"} top-8 ${alignment == "right" ? "right-0" : "left-0"} mx-8 md:block md:mx-[calc(60dvw-2.5rem)] lg:top-15 xl:top-10 xl:mx-[calc(60dvw-6rem)] 2xl:top-14`}
       >
-        {team.split(" ").length > 2 ? (
-          <>
-            {/* puts sports that are two words or more in one line */}
-            {/* putting the non breaking space (\u00a0) in json breaks the font (Gotham Condensed) */}
-            {team.split(" ").slice(0, 1)}
-            <p className="w-max">{team.split(" ").slice(1).join(" ")}</p>
-          </>
-        ) : (
-          team
-        )}
-      </motion.h2>
+        <motion.h2
+          initial={{
+            translateX: alignment == "right" ? "100%" : "-100%",
+          }}
+          whileInView={{
+            transition: { duration: 0.75 },
+            translateX: "0",
+          }}
+        >
+          {team.split(" ").length > 2 ? (
+            <>
+              {/* puts sports that are two words or more in one line */}
+              {/* putting the non breaking space (\u00a0) in json breaks the font (Gotham Condensed) */}
+              {team.split(" ").slice(0, 1)}
+              <p className="w-max">{team.split(" ").slice(1).join(" ")}</p>
+            </>
+          ) : (
+            team
+          )}
+        </motion.h2>
+      </div>
       <div
         className={`relative text-center p-8 text-uaap-blue flex-4 flex flex-col justify-between h-full sm:p-15 md:p-8 ${alignment == "right" ? "md:text-right" : "md:text-left"} lg:py-15 xl:px-20 xl:py-10 xl:self-center 2xl:py-14`}
       >
-        <div
+        {/* <div
           className={`absolute h-[50dvh] w-8 top-0 ${alignment == "right" ? "right-0" : "left-0"} bg-white z-10 xl:w-20`}
-        />
-        <motion.div
-          initial="initial"
-          whileInView="view"
-          variants={variants.text.container}
-        >
-          <h2 className="capitalize font-display font-bold text-[2rem] mb-2 md:hidden">
-            {team}
-          </h2>
-          <motion.h2
-            variants={variants.text.children}
-            className="uppercase text-[3.75rem] font-display font-bold leading-[85%] mb-2 sm:text-[4rem] md:text-[4.5rem] lg:text-[5rem] xl:text-[6rem]"
+        /> */}
+        <div className="overflow-hidden">
+          <motion.div
+            initial="initial"
+            whileInView="view"
+            variants={variants.text.container}
           >
-            {title}
-          </motion.h2>
-          <motion.p
-            variants={variants.text.children}
-            className="text-sm font-body max-w-96 lg:text-base m-auto md:max-w-none"
-          >
-            <span className="font-bold uppercase">{firstTwoBlurb} </span>
-            {restBlurb}
-          </motion.p>
-        </motion.div>
+            <h2 className="capitalize font-display font-bold text-[2rem] mb-2 md:hidden">
+              {team}
+            </h2>
+            <motion.h2
+              variants={variants.text.children}
+              className="uppercase text-[3.75rem] font-display font-bold leading-[85%] mb-2 sm:text-[4rem] md:text-[4.5rem] lg:text-[5rem] xl:text-[6rem]"
+            >
+              {title}
+            </motion.h2>
+            <motion.p
+              variants={variants.text.children}
+              className="text-sm font-body max-w-96 lg:text-base m-auto md:max-w-none"
+            >
+              <span className="font-bold uppercase">{firstTwoBlurb} </span>
+              {restBlurb}
+            </motion.p>
+          </motion.div>
+        </div>
         <NavLink
           to={`/${slug}`}
           className={`self-center mt-4 ${alignment == "right" ? "md:self-end" : "md:self-start"}`}
