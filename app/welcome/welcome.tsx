b39def802649a7a2ec26d061c70cfce7cdb6bcbc
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-08 12:12:41 +0800

    create wrapper component for team section

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,34 +1,5 @@
-import { TeamSectionHalf } from "~/components/team-section-half";
-import { TeamSectionFull } from "~/components/team-section-full";
-import { Credits } from "~/components/credits";
-import data from "../assets/data/data.json";
+import { TeamSection } from "~/pages/TeamSection";
 
 export function Welcome() {
-  return (
-    <div className="snap-y snap-mandatory overflow-y-scroll h-dvh">
-      {Object.entries(data).map(([key, { team, title, article }], idx) =>
-        key == "cheerdance" ? (
-          <>
-            <TeamSectionFull
-              key={key}
-              team={team}
-              title={title}
-              blurb={article.split("\n\n")[0]} // get first paragraph of article
-              slug={key}
-            />
-          </>
-        ) : (
-          <TeamSectionHalf
-            key={key}
-            team={team}
-            title={title}
-            blurb={article.split("\n\n")[0]} // get first paragraph of article
-            alignment={idx % 2 == 0 ? "right" : "left"}
-            slug={key}
-          />
-        )
-      )}
-      <Credits />
-    </div>
-  );
+  return <TeamSection />;
 }
