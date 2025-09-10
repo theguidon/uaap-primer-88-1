ca92129fe56a478f802e77a9e7b73975d9a48ba3
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-09-10 23:30:32 +0800

    sidebar styled

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,9 +1,10 @@
 import styles from "./sidebar.module.css";
 import { easeInOut, motion } from "motion/react";
 import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
-export default function SideBar() {
-	const selected = useState(-1);
+export default function SideBar(visible: boolean, setVisibility: (visible: boolean) => void) {
+	const [selected, setSelected] = useState(-1);
 	const links = [
 		{ name: "Cheerdance", sublinks: false },
 		{ name: "Badminton", sublinks: true },
@@ -14,8 +15,27 @@ export default function SideBar() {
 		{ name: "Tennis", sublinks: true },
 		{ name: "Track and Field", sublinks: true }
 	]
+
+	function select(hasLinks: boolean, ix: number) {
+		setSelected(hasLinks ? ix : -1);
+	}
 	return (
-		<nav>
+		<nav
+			style={{
+				left: (visible ? 0 : "-100%")
+			}}
+		>
+			<button
+				style={{
+					marginLeft: 40,
+					marginBottom: 28,
+					cursor: "pointer"
+				}}
+
+				onClick={() => setVisibility(false)}
+			>
+				<IoClose color="white" size={32} />
+			</button>
 			<section className={styles.title}>
 				<div>
 					<motion.h1
@@ -56,21 +76,25 @@ export default function SideBar() {
 				</div>
 			</section>
 			<ul>
-				{links.map((link) => (
-					<li>
-						<section className={styles.navLinkTitle}>
+				{links.map((link, ix) => (
+					<li className={selected == ix ? styles.selected : ''}>
+						<section
+							className={styles.navLinkTitle}
+							onClick={() => select(link.sublinks, ix)}
+						>
 							<p>{link.name}</p>
 							{link.sublinks && <img src="/keyboard_arrow_down.svg" />}
 						</section>
-						{link.sublinks &&
+						{
+							link.sublinks &&
 							<ul>
-								<li>Men's</li>
-								<li>Women's</li>
+								<li>Men's {link.name}</li>
+								<li>Women's {link.name}</li>
 							</ul>
 						}
 					</li>
 				))}
 			</ul>
-		</nav>
+		</nav >
 	);
 }
