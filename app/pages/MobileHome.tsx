7ec6d28e167c9be59708bc588ea21cbf5c3956cf
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-12 16:34:39 +0800

    yeah

--- app/pages/MobileHome.tsx
+++ app/pages/MobileHome.tsx
@@ -58,18 +58,18 @@ export default function MobileHome() {
         <div
           className={styles.bloom}
           style={{
-            width: '90vw',
-            height: '90vw',
-            position: 'absolute',
+            width: "90vw",
+            height: "90vw",
+            position: "absolute",
             left: 0,
             top: 50,
-            zIndex: 20
+            zIndex: 20,
           }}
         >
           <BloomingCircles
             outerSize="100%"
             radiiGap="10%"
-            colors={['#b7e1f2', '#56b8e1', '#0e9ad4']}
+            colors={["#b7e1f2", "#56b8e1", "#0e9ad4"]}
             animStagger={25}
             animSpeed={0.75}
             delay={0}
@@ -78,18 +78,22 @@ export default function MobileHome() {
         <div
           className={styles.bloom}
           style={{
-            width: '60vw',
-            height: '60vw',
-            position: 'absolute',
+            width: "60vw",
+            height: "60vw",
+            position: "absolute",
             right: 0,
             bottom: 0,
-            zIndex: 20
+            zIndex: 20,
           }}
         >
           <BloomingCircles
             outerSize="100%"
             radiiGap="10%"
-            colors={['rgba(249, 218, 44, 0.3)', 'rgba(249, 218, 44, 0.7)', 'rgba(249, 218, 44, 1)']}
+            colors={[
+              "rgba(249, 218, 44, 0.3)",
+              "rgba(249, 218, 44, 0.7)",
+              "rgba(249, 218, 44, 1)",
+            ]}
             animStagger={25}
             animSpeed={0.6}
             delay={15}
@@ -105,41 +109,74 @@ export default function MobileHome() {
           centeredSlides={true}
           autoplay={{
             delay: 1500,
-            disableOnInteraction: false
+            disableOnInteraction: false,
           }}
           modules={[Autoplay]}
         >
           {images.map((image) => (
             <SwiperSlide>
-              <img
-                style={{
-                  height: "100%",
-                  objectFit: "contain"
-                }}
-                src={`/homepage/${image}`}
-              />
+              {image == "mfootball.png" ? (
+                <img
+                  className="h-full object-contain mt-10"
+                  src={`/homepage/${image}`}
+                />
+              ) : (
+                <img
+                  style={{
+                    height: "100%",
+                    objectFit: "contain",
+                  }}
+                  src={`/homepage/${image}`}
+                />
+              )}
             </SwiperSlide>
           ))}
         </Swiper>
-        <motion.div className={styles.underRibbon}
+        <motion.div
+          className={styles.underRibbon}
           initial={{ width: 0 }}
           animate={{ width: "100%" }}
-          style={{ overflowX: "clip", height: "100%", position: "absolute", zIndex: 25, right: 0, bottom: 0 }}
+          style={{
+            overflowX: "clip",
+            height: "100%",
+            position: "absolute",
+            zIndex: 25,
+            right: 0,
+            bottom: 0,
+          }}
         >
           <img
             src="/underphoto_mobile.svg"
-            style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "right center" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: "right center",
+            }}
           />
         </motion.div>
-        <motion.div className="over-ribbon"
+        <motion.div
+          className="over-ribbon"
           initial={{ width: 0 }}
           animate={{ width: "100vw" }}
           transition={{ delay: 0.65 }}
-          style={{ overflowX: "clip", height: 200, position: "absolute", zIndex: 575, right: 0, bottom: 0 }}
+          style={{
+            overflowX: "clip",
+            height: 200,
+            position: "absolute",
+            zIndex: 575,
+            right: 0,
+            bottom: 0,
+          }}
         >
           <img
             src="/overphoto_mobile.svg"
-            style={{ width: "100vw", height: "100%", objectFit: "cover", objectPosition: "right center" }}
+            style={{
+              width: "100vw",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: "right center",
+            }}
           />
         </motion.div>
       </section>
