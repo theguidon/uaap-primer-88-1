9c4a00f15a50fdc466a4568561c3c3be9448f8a7
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-13 12:41:47 +0800

    vercel fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   // dev base
-  base: "/uaap-88-primer-first-semester",
+  // base: "/uaap-88-primer-first-semester",
   // prod base
   // base: "https://theguidon.com/uaap-88-primer-first-semester/",
 });
