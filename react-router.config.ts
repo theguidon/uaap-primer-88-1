0c4d469696859c32a5b8f75edbe8cbec3cc881ca
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-13 12:40:25 +0800

    vercel dev env

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: "/uaap-88-primer-first-semester",
+  // basename: "/uaap-88-primer-first-semester",
 } satisfies Config;
